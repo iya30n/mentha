6e72ee7f6ad00779cc8ca52a883ea303c95c05e5
iya30n <varmeziyaryasin@gmail.com>
2023-02-14 15:13:20 +0330

    load labels and model

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module mentha
 
 go 1.19
+
+require (
+	github.com/galeone/tensorflow/tensorflow/go v0.0.0-20221023090153-6b7fa0680c3e
+	github.com/galeone/tfgo v0.0.0-20221023090852-d89a5c7e31e1
+)
+
+require google.golang.org/protobuf v1.28.1 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,64 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"image"
+	"log"
+	"os"
+
+	tg "github.com/galeone/tfgo"
+)
+
+var (
+	model  *tg.Model
+	labels []string
+
+	modelPath string = "/your/path"
+	imagePath string = "/your/path"
+)
 
 func main() {
-	fmt.Println("Hi i'm mentha")
+	os.Setenv("TF_CPP_MIN_LOG_LEVEL", "2")
+
+	loadLabels(modelPath)
+	model = tg.LoadModel(modelPath, []string{"serve"}, nil)
+	img, err := loadImage(imagePath)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	mainHandler(img)
 }
+
+func loadLabels(path string) error {
+	modelLabels := path + "/labels.txt"
+	f, err := os.Open(modelLabels)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		labels = append(labels, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func loadImage(fileName string) (image.Image, error) {
+	f, err := os.Open(fileName)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	img, _, err := image.Decode(f)
+
+	return img, err
+}
+
+func mainHandler(img image.Image) {}
